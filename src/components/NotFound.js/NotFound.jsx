4db5af058514df52ac31d02c99cc02f04e1276b9
Stray Bear <gohan9511@gmail.com>
2022-11-01 01:31:51 +0900

    Feature ( NotFound.jsx ) : Error Page
    Add Not Found Page

--- src/components/NotFound.js/NotFound.jsx
+++ src/components/NotFound.js/NotFound.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import styled from "styled-components";
+import disConnect from "../../asset/disconnect.png";
 
 const NotFound = () => {
   return (
-    <>
-      <div>Not Found Page</div>
-      <div>Sub Title</div>
-    </>
+    <DisconnectBox>
+      <Disconnected />
+    </DisconnectBox>
   );
 };
+const DisconnectBox = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
+const Disconnected = styled.img.attrs({ src: disConnect })`
+  background-repeat: no-repeat;
+  background-position: top center;
+  background-size: cover;
+  background-attachment: fixed;
+  width: 100%;
+  height: 100%;
+`;
 
 export default NotFound;
